ef303cc42dc610a27b2b6954d370300ed36f76a3
ywatch <94308024+ywatch@users.noreply.github.com>
2024-04-30 18:41:17 +0100

    login page 70%

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule,HttpErrorResponse} from '@angular/common/http';
+import { Component} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-
+export class LoginComponent{
+loginObj:login;
+constructor(private http:HttpClient,private router:Router){
+  this.loginObj=new login();
+}
+onLogin(){
+this.http.get(`http://localhost:8080/user/login?username=${this.loginObj.Name}&password=${this.loginObj.password}`,{responseType:'text'}).pipe(
+  catchError((error: HttpErrorResponse) => {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError('Something bad happened; please try again later.');
+  })
+).subscribe(response=>{
+  alert(response);
+  this.router.navigateByUrl('/dashboard');
+},
+error=>{
+  console.error(error);
+});
+}
+}
+export class login {
+  Name:string;
+  password:string;
+  constructor(){
+    this.Name='';
+    this.password='';
+  }
 }
