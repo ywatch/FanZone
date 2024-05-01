ad5bc69ee0946e050c1c57e1af0c4fa77a7d3981
ywatch <94308024+ywatch@users.noreply.github.com>
2024-05-01 18:32:04 +0100

    navbar100%+logo implementation

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
